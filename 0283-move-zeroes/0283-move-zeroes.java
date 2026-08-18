59f008db071ef5d1731ac627c31843b94c0531b4
gauravsinghbora121 <gauravsinghbora121@gmail.com>
2026-08-18 10:56:46 +0530

    Time: 2 ms (92.04%), Space: 47.6 MB (76.86%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int low=0;
+
+       for(int high=0;high<nums.length;high++){
+        if(nums[high]!=0){
+
+            int temp=nums[high];
+             nums[high]=nums[low];
+             nums[low]=temp;
+             low++;
+             
+        }
+       }
+       
+        
+        
+    }
+}
